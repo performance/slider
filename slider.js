acc2c8cc1f132613b0384710fa00b73b93ad1d1d
performance <sriram.durbha@gmail.com>
2014-12-13 05:59:47 -0800

    renamed and put the correct files in place
    some cleanup + issue list as comments

--- slider.js
+++ slider.js
@@ -1,6 +1,5 @@
 // ref:
 // http://www.sitepoint.com/image-manipulation-with-html5-canvas-a-sliding-puzzle-2/
-// http://www.sitepoint.com/image-manipulation-with-html5-canvas-a-sliding-puzzle-2/
 // http://www.homeandlearn.co.uk/JS/html5_canvas_keyboard_keys.html
 // http://www.ibm.com/developerworks/library/wa-games/
 
@@ -9,203 +8,276 @@
 // enhancements:
 // 0. randomize the image with an input seed [ default 0 ]
 // 1. display the target to the right of the puzzle
-// 2. another input area to enter desired moves
+// 2. another input area to enter desired moves  <-- DONE
 // 3. button to play it back.
 // 4. some way to control playback speed
 // 5. track speeds, time taken etc.
 // 6. ability to change the image.
 // 7. share with friends
-var puzzle_context = document.getElementById('puzzle').getContext('2d');
-var target_context = document.getElementById('target').getContext('2d');
+
+// alert( "slide.js loaded ");
+
+var context = document.getElementById('puzzle').getContext('2d');
+
+var target_context = document.getElementById('puzzle_final').getContext('2d');
+
 var img = new Image();
-// img.src = 'https://www.jigsawexplorer.com/puzzles/subjects/lotus-flower-383x300.jpg'
-img.addEventListener('load', onImageLoad, false);
-
 img.src = 'http://tcktcktck.org/wp-content/uploads/2013/01/3226723901_1ca63f09cb_b.jpg'
-// img.src = 'http://www.brucealderman.info/Images/dimetrodon.jpg';
-// img.addEventListener('load', drawTiles, false);
 // web search for creative commons images of flowers
+// 'http://www.brucealderman.info/Images/dimetrodon.jpg';
+img.addEventListener('load', drawOnImageLoad, false);
 
-var boardSize = document.getElementById('puzzle').width;
-var numTiles = 3; // document.getElementById('scale').value;
+var puzzleWidth = document.getElementById('puzzle').width;
+var tilesPerSide = 3; // document.getElementById('scale').value;
 
-var tileSize = boardSize / numTiles;
+var tileSize = puzzleWidth / tilesPerSide;
 
-var selectedTile = new Object;
-selectedTile.x = 0;
-selectedTile.y = 0;
+var clickLoc = new Object;
+clickLoc.x = 0;
+clickLoc.y = 0;
 
-var blankTile = new Object;
-blankTile.x = 0;
-blankTile.y = 0;
+var emptyLoc = new Object;
+emptyLoc.x = 0;
+emptyLoc.y = 0;
 
 var solved = false;
 
 var gridOfTiles;
 createGridOfTiles();
-drawTiles();
+renderGridOfTiles();
 
 
-
-
-window.addEventListener( "keypress", doKeyDown, false );
-
-function doKeyDown( e )
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement.offsetLeft
+document.getElementById('puzzle').onclick = function(e)
 {
-  // alert( "hi neelu : " + e + " code is : " + e.keyCode  );
+  document.getElementById("puzzle").focus();
+  clickLoc.x = Math.floor((e.pageX - this.offsetLeft) / tileSize);
+  clickLoc.y = Math.floor((e.pageY - this.offsetTop) / tileSize);
+  var input = neighbor( clickLoc, emptyLoc );
+  if ( input.isNeighbor )
+  {
+    executeMove( input.move )
+    // swapTiles( emptyLoc, clickLoc );
+    renderGridOfTiles();
+  }
+  showSolvedMessage();
+};
+
+function neighbor( clickLoc, emptyLoc )
+{
+  var isNeighbor = false;
+  var move = '';
+  var distance = Math.abs(clickLoc.x - emptyLoc.x) + Math.abs(clickLoc.y - emptyLoc.y);
+  if ( 1 == distance )
+    isNeighbor = true;
+
+  if ( isNeighbor )
+  {
+    if( clickLoc.x > emptyLoc.x )
+    {
+      move = 'L';
+    }
+    else if ( clickLoc.x < emptyLoc.x )
+    {
+      move = 'H';
+    }
+    else if ( clickLoc.y > emptyLoc.y )
+    {
+      move = 'J';
+    }
+    else if ( clickLoc.y < emptyLoc.y )
+    {
+      move = 'K';
+    }
+  }
+  return { isNeighbor : isNeighbor, move: move };
+}
 
 
+// function handleKeyPressed( event )
+// window.addEventListener( "keypress", handleKeyPressed, false );
 
-         if ( 108 == e.keyCode )
+// http://www.w3.org/WAI/WCAG20/Techniques/working-examples/SCR29/action-on-div.html
+document.getElementById("main").onkeyup = handleOnKeyUp;
+document.getElementById("input_moves").onkeyup = handleOnKeyUp;
+
+function handleOnKeyUp(event)
+{
+  // alert( " in on key up handler ");
+  var e = event;
+  if ( window.event )
   {
-    selectedTile.x = blankTile.x + 1;
-    selectedTile.y = blankTile.y;
-  } else if ( 107 == e.keyCode )
-  {
-    selectedTile.x = blankTile.x;
-    selectedTile.y = blankTile.y - 1;
-  } else if ( 106 == e.keyCode )
-  {
-    selectedTile.x = blankTile.x;
-    selectedTile.y = blankTile.y + 1;
-  } else if ( 104 == e.keyCode )
-  {
-    selectedTile.x = blankTile.x - 1;
-    selectedTile.y = blankTile.y;
+    e = window.event;
   }
 
-  // alert( " blankTile (" + blankTile.x + "," + blankTile.y + ")" +
-  //        " selectedTile (" + selectedTile.x + "," + selectedTile.y + ")"
-  //      );
+  // alert( e.keyCode );
 
-  swapTiles( blankTile, selectedTile );
-  drawTiles();
-  if (solved) {
-    setTimeout(function() {alert("You solved it!");}, 500);
+  if ( ( 106 == e.keyCode ) || ( 74 == e.keyCode ) ) // 'J'
+  {
+    if ( emptyLoc.y < ( tilesPerSide -1 ) )
+    {
+      clickLoc.x = emptyLoc.x;
+      clickLoc.y = emptyLoc.y + 1;
+    }
+  }
+  else if ( ( 107 == e.keyCode ) || ( 75 == e.keyCode ) ) // 'K'
+  {
+    if ( 0 < emptyLoc.y )
+    {
+      clickLoc.x = emptyLoc.x;
+      clickLoc.y = emptyLoc.y - 1;
+    }
+  }
+  else if ( ( 104 == e.keyCode ) || ( 72 == e.keyCode ) ) // 'H'
+  {
+    if ( emptyLoc.x > 0 )
+    {
+      clickLoc.x = emptyLoc.x - 1;
+      clickLoc.y = emptyLoc.y;
+    }
+  }
+  else if ( ( 108 == e.keyCode ) || ( 76 == e.keyCode ) ) // 'L'
+  {
+    if ( emptyLoc.x < ( tilesPerSide -1 ) )
+    {
+      clickLoc.x = emptyLoc.x + 1;
+      clickLoc.y = emptyLoc.y;
+    }
+  }
+  var input = neighbor( clickLoc, emptyLoc );
+  if ( input.isNeighbor )
+  {
+    executeMove( input.move );
+    renderGridOfTiles();
+  }
+  showSolvedMessage();
+}
+
+function showSolvedMessage()
+{
+  // alert( " puzzle state is: " + solved );
+  if (solved)
+  {
+    var moves_made = document.getElementById("moves_made").innerHTML;
+    setTimeout(
+      function()
+      {
+        alert("You solved it! in " + moves_made.length - 1 );
+      },
+      500
+    ); // wait half a second
   }
 }
 
-document.getElementById('puzzle').onclick = function(e) {
-  selectedTile.x = Math.floor((e.pageX - this.offsetLeft) / tileSize);
-  selectedTile.y = Math.floor((e.pageY - this.offsetTop) / tileSize);
-  if ( neighbours( blankTile, selectedtile ) ) {
-    swapTiles(blankTile, selectedTile);
-    drawTiles();
-  }
-  if (solved) {
-    setTimeout(function() {alert("You solved it!");}, 500);
-  }
-};
-
 function createGridOfTiles() {
-  // alert( "board set " + numTiles );
-
-  gridOfTiles = new Array(numTiles);
-  for (var i = 0; i < numTiles; ++i) {
-    gridOfTiles[i] = new Array(numTiles);
-    for (var j = 0; j < numTiles; ++j) {
+  gridOfTiles = new Array(tilesPerSide);
+  // alert( " gridOfTiles allocated ");
+  for (var i = 0; i < tilesPerSide; ++i) {
+    gridOfTiles[i] = new Array(tilesPerSide);
+    for (var j = 0; j < tilesPerSide; ++j) {
       gridOfTiles[i][j] = new Object;
-      gridOfTiles[i][j].x = (numTiles - 1) - i;
-      gridOfTiles[i][j].y = (numTiles - 1) - j;
+      gridOfTiles[i][j].x = (tilesPerSide - 1) - i;
+      gridOfTiles[i][j].y = (tilesPerSide - 1) - j;
     }
   }
-  blankTile.x = gridOfTiles[numTiles - 1][numTiles - 1].x;
-  blankTile.y = gridOfTiles[numTiles - 1][numTiles - 1].y;
+  emptyLoc.x = gridOfTiles[tilesPerSide - 1][tilesPerSide - 1].x;
+  emptyLoc.y = gridOfTiles[tilesPerSide - 1][tilesPerSide - 1].y;
   solved = false;
 }
 
-function drawTiles() {
-  // alert( "drawing tiles");
-  puzzle_context.clearRect ( 0 , 0 , boardSize , boardSize );
-  for (var i = 0; i < numTiles; ++i) {
-    for (var j = 0; j < numTiles; ++j) {
+function renderGridOfTiles() {
+  context.clearRect ( 0 , 0 , puzzleWidth , puzzleWidth );
+  for (var i = 0; i < tilesPerSide; ++i) {
+    for (var j = 0; j < tilesPerSide; ++j) {
       var x = gridOfTiles[i][j].x;
       var y = gridOfTiles[i][j].y;
-      if(i != blankTile.x || j != blankTile.y || solved == true) {
-        // api doc from http://www.w3schools.com/tags/canvas_drawimage.asp
-        puzzle_context.drawImage(
-          img,          // img              Specifies the image, canvas, or video element to use
-          x * tileSize, // sx      Optional. The x coordinate where to start clipping
-          y * tileSize, // sy      Optional. The y coordinate where to start clipping
-          tileSize,     // swidth  Optional. The width  of the clipped image
-          tileSize,     // sheight Optional. The height of the clipped image
-          i * tileSize, // x                 The x coordinate where to place the image on the canvas
-          j * tileSize, // y                 The y coordinate where to place the image on the canvas
-          tileSize,     // width   Optional. The width  of the image to use (stretch or reduce the image)
-          tileSize      // height  Optional. The height of the image to use (stretch or reduce the image)
-        );
+      if(i != emptyLoc.x || j != emptyLoc.y || solved == true) {
+        context.drawImage(img, x * tileSize, y * tileSize, tileSize, tileSize,
+            i * tileSize, j * tileSize, tileSize, tileSize);
       }
     }
   }
 }
 
-
-
-function onImageLoad() {
-
-  console.log( "image loaded ");
-  createGridOfTiles();
-  drawTiles();
-  console.log( "tiles drawn " );
-  target_context.drawImage( img, 0, 0 );
-  console.log( "target drawn " );
-
-
+function drawOnImageLoad() {
+  renderGridOfTiles();
+  target_context.clearRect ( 0 , 0 , puzzleWidth , puzzleWidth );
+  target_context.drawImage(img, 0,0 );
 }
 
-
-function neighbours( emptyTile, selectedTile )
+function executeMove( move )
 {
-  var x1 = emptyTile.x;
-  var x2 = selectedTile.x;
-  var y1 = emptyTile.y;
-  var y2 = selectedTile.y;
-  var distance = Math.abs(x1 - x2) + Math.abs(y1 - y2);
-  var flag = ( 1 == distance );
-  return flag;
-
-}
-
-function swapTiles(emptyTile, neighbouringTile) {
   if (!solved) {
-    var keyPressed = '';
-    gridOfTiles[emptyTile.x][emptyTile.y].x = gridOfTiles[neighbouringTile.x][neighbouringTile.y].x;
-    gridOfTiles[emptyTile.x][emptyTile.y].y = gridOfTiles[neighbouringTile.x][neighbouringTile.y].y;
-    gridOfTiles[neighbouringTile.x][neighbouringTile.y].x = numTiles - 1;
-    gridOfTiles[neighbouringTile.x][neighbouringTile.y].y = numTiles - 1;
-    // check which way they moved.
-    // this needs to be decoupled and tested.
-    if ( emptyTile.x > neighbouringTile.x )
+    var blankTile   = emptyLoc;
+    var selectedTile = clickLoc;
+    if ( 'H' == move )
     {
-      keyPressed = 'H';
+      selectedTile.x = blankTile.x - 1;
+      selectedTile.y = blankTile.y    ;
     }
-    else if ( emptyTile.x < neighbouringTile.x )
+    else if ( 'L' == move )
     {
-      keyPressed = 'L';
+      selectedTile.x = blankTile.x + 1;
+      selectedTile.y = blankTile.y    ;
     }
-    if ( emptyTile.y > neighbouringTile.y )
+    else if ( 'J' == move )
     {
-      keyPressed = 'K';
+      selectedTile.x = blankTile.x    ;
+      selectedTile.y = blankTile.y + 1;
     }
-    else if ( emptyTile.y < neighbouringTile.y )
+    else if ( 'K' == move )
     {
-      keyPressed = 'J';
+      selectedTile.x = blankTile.x    ;
+      selectedTile.y = blankTile.y - 1;
     }
-    document.getElementById("moves_made").innerHTML += keyPressed;
-    emptyTile.x = neighbouringTile.x;
-    emptyTile.y = neighbouringTile.y;
-    checkIfSolved();
-  }
-}
 
+    swapTiles(blankTile, selectedTile);
+    document.getElementById("moves_made").innerHTML += move;
+  }
+
+}
 function checkIfSolved() {
   var flag = true;
-  for (var i = 0; i < numTiles; ++i) {
-    for (var j = 0; j < numTiles; ++j) {
+  for (var i = 0; ( ( flag ) && ( i < tilesPerSide ) ); ++i) {
+    for (var j = 0; j < tilesPerSide; ++j) {
       if (gridOfTiles[i][j].x != i || gridOfTiles[i][j].y != j) {
+//        alert( " i = " + i  + " j = " + j + "gtx,y = " + gridOfTiles[i][j].x + "," + gridOfTiles[i][j].y );
         flag = false;
+        break;
       }
     }
   }
   solved = flag;
 }
+
+
+function swapTiles(blankTile, selectedTile) {
+  if (!solved) {
+    gridOfTiles[blankTile.x][blankTile.y].x = gridOfTiles[selectedTile.x][selectedTile.y].x;
+    gridOfTiles[blankTile.x][blankTile.y].y = gridOfTiles[selectedTile.x][selectedTile.y].y;
+    gridOfTiles[selectedTile.x][selectedTile.y].x = tilesPerSide - 1;
+    gridOfTiles[selectedTile.x][selectedTile.y].y = tilesPerSide - 1;
+    blankTile.x = selectedTile.x;
+    blankTile.y = selectedTile.y;
+    checkIfSolved();
+    // var keyPressed = '';
+    // if ( blankTile.x > selectedTile.x )
+    // {
+    //   keyPressed = 'H';
+    // }
+    // else if ( blankTile.x < selectedTile.x )
+    // {
+    //   keyPressed = 'L';
+    // }
+    // else if ( blankTile.y > selectedTile.y )
+    // {
+    //   keyPressed = 'K';
+    // }
+    // else if ( blankTile.y < selectedTile.y )
+    // {
+    //   keyPressed = 'J';
+    // }
+    // document.getElementById("moves_made").innerHTML += keyPressed;
+  }
+}
+
